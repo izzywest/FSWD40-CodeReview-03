93770618b4f15d2605c9f66c56017e00905c173b
schahnaz <schahnaz@mail.de>
2018-04-28 09:13:32 +0200

    add calculation for italy and some other things

--- js/script.js
+++ js/script.js
@@ -11,24 +11,20 @@ document.getElementById("calc").onclick = function() {
 	var hpUsr = document.getElementById("inputHp").value;
 
 	if (document.getElementById("rd1").checked == true) {
-		//alert("Austria checked");
 		insCost = hpUsr * 100 / ageUsr + 50;
 	} else if (document.getElementById("rd2").checked == true) {
-		//alert("Germany");
 		insCost = hpUsr * 120 / ageUsr + 100;
 	} else if (document.getElementById("rd3").checked == true) {
-		//alert("Italy");
-		ageUsr1 = ageUsr + 2;
+		ageUsr1 = parseFloat(ageUsr) + parseFloat(2);
 		insCost = hpUsr * 150 / ageUsr1 + 150;
-		console.log("inscost: " + insCost);
-		console.log("ageUsr1: " + ageUsr1);
-		console.log("ageUsr: " + ageUsr);
 	}
 
 	var insCost;
-	var result =  "Hey " + nameUsr + ", when you are " + ageUsr + " years old and your car has " + hpUsr + " horse-power, then you have to pay " + insCost + " €.";
+	insCost1 = Math.round(insCost * 100) / 100;
+	var result =  "Hey " + nameUsr + ", when you are " + ageUsr 
+	+ " years old and your car has " + hpUsr 
+	+ " horse-power, then you have to pay " + insCost1 + " €.";
 
 document.getElementById("outputName").innerHTML = result;
-
 
 }
